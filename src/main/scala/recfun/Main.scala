d4813cabda89d6d7f45ecc9876b02f3570de960d
Ivan Garcia <ivan.garcia@unosquare.com>
2014-09-29 13:00:55 -0500

    Third

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -38,6 +38,8 @@ object Main {
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    
+    if (money==0) 1
+    else if (coins.isEmpty || money<0) 0
+    else countChange(money-coins.head, coins) + countChange(money, coins.tail)
   }
 }
